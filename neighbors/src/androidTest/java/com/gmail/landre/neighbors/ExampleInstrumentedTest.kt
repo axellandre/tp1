6ae1d4129884d56864a9b55663406407a7b40c9f
sheeeeesh <61969570+sheeeeesh@users.noreply.github.com>
2021-11-06 22:06:12 +0100

    update final

---
+++ neighbors/src/androidTest/java/com/gmail/landre/neighbors/ExampleInstrumentedTest.kt
@@ -0,0 +1,26 @@
+package com.gmail.landre.neighbors
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.* // ktlint-disable no-wildcard-imports
+import org.junit.Test
+import org.junit.runner.RunWith
+
+// ktlint-disable no-wildcard-imports
+
+// ktlint-disable no-wildcard-imports
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.gmail.landre.neighbors", appContext.packageName)
+    }
+}
---
+++ neighbors/src/main/java/com/gmail/landre/neighbors/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.gmail.landre.neighbors
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.gmail.landre.neighbors.fragments.ListNeighborsFragment
+
+class MainActivity : AppCompatActivity(), NavigationListener {
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        showFragment(ListNeighborsFragment())
+    }
+
+    override fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            addToBackStack(null)
+        }.commit()
+    }
+
+    override fun updateTitle(title: Int) {
+        toolbar.setTitle(title)
+    }
+}
